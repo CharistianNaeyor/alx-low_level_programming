400798b8192674919897914244b6d06a03e4d295
CharistianNaeyor <engrnaeyor@gmail.com>
2023-10-12 15:37:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int_putchar(int c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -11,3 +13,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
