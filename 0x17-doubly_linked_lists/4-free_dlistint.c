35c4d878ac859aa92aaefaa76aab2de699738148
CharistianNaeyor <engrnaeyor@gmail.com>
2024-01-12 16:38:41 -0800

    4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,13 +7,13 @@
  **/
 void free_dlistint(dlistint_t *head)
 {
-        if (head == NULL)
-        return;
+	if (head == NULL)
+	return;
 
-        while (head->next)
-        {
-        head = head->next;
-        free(head->prev);
-        }
-        free(head);
+	while (head->next)
+	{
+	head = head->next;
+	free(head->prev);
+	}
+	free(head);
 }
